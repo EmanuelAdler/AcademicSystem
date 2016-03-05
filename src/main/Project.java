b1937d19572acc6f065b9ab30d8b049644a8dcfb
Andreferro <andreferro@ic.ufal.br>
2016-03-04 21:04:12 -0300

    Add allocateUser method

--- src/main/Project.java
+++ src/main/Project.java
@@ -7,7 +7,7 @@ public class Project {
 	protected String title;
 	protected String objective;
 	protected String description;
-	protected String professor;
+	protected ArrayList<String> professorList;
 	protected String fundingAgency;
 	protected Double financedAmount;
 	protected String startDate;
@@ -18,11 +18,11 @@ public class Project {
 	
 	
 	
-	public Project(String title, String objective, String description, String professor, String fundingAgency, Double financedAmount, String startDate, String endDate, boolean isDone, String status){
+	public Project(String title, String objective, String description, String fundingAgency, Double financedAmount, String startDate, String endDate, boolean isDone, String status){
 		this.title = title;
 		this.objective = objective;
 		this.description = description;
-		this.professor = professor;
+		this.professorList = new ArrayList<String>();
 		this.fundingAgency = fundingAgency;
 		this.financedAmount = financedAmount;
 		this.isDone = isDone;
@@ -32,6 +32,59 @@ public class Project {
 		this.team = new ArrayList<User>();
 	}
 	
+	public void allocateUser(User newUser){
+		if(this.status != "em andamento"){
+			if(newUser instanceof Professor){
+				if(this.team.size() == 0){
+					/*
+					 * Um projeto precisa de no minimo um professor.
+					 * Verifica se o novo usuário é um professor.
+					 */
+					if(newUser instanceof Professor){
+						this.team.add(newUser);
+						this.professorList.add(newUser.getName());
+					}
+					else{
+						System.out.println("Este projeto não possui um professor cadastrado");
+					}
+				}
+				else{
+					/*
+					 * Verifica se o usuário já está no projeto.
+					 */
+					for(int i = 0; i < this.team.size(); i++){
+						if(this.team.get(i) == newUser){
+							System.out.println("Este usuário já está no projeto");
+							break;
+						}
+						else if(this.team.get(i) != newUser && i == this.team.size()-1){
+							if(newUser instanceof Student){
+								/*
+								 * Um estudante não pode estar em mais de dois projetos "em andamento"
+								 */
+								int quantity = 0;
+								for(i = 0; i < newUser.getProjects().size(); i++){
+									if(newUser.getProjects().get(i).getStatus() == "em andamento"){
+										quantity++;
+										if(quantity == 2)
+											System.out.println("Este aluno não pode participar deste projeto");
+										else if(quantity < 2 && i == newUser.getProjects().size()-1)
+											this.team.add(newUser);
+									}
+									else{
+										this.team.add(newUser);
+									}
+								}
+							}
+						}
+					}
+					
+				}
+			}
+		}
+		else
+			System.out.println("Este projeto já está em andamento");
+	}
 	
 	
 	public String getTitle() {
@@ -52,11 +105,8 @@ public class Project {
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	public String getProfesor() {
-		return professor;
-	}
-	public void setProfesor(String profesor) {
-		this.professor = profesor;
+	public ArrayList<String> getProfesor() {
+		return professorList;
 	}
 	public String getFundingAgency() {
 		return fundingAgency;
